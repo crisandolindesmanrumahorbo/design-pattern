4149e32d44a0308e880cde254e264117d78660d6
Crisandolin Rumahorbo <crisandolin.rumahorbo@btpn.com>
2022-07-29 19:46:04 +0700

    increment an array

--- src/test/java/com/rumahorbo/app/AppTest.java
+++ src/test/java/com/rumahorbo/app/AppTest.java
@@ -2,6 +2,9 @@ package com.rumahorbo.app;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
@@ -26,6 +29,45 @@ public class AppTest {
         int actual = solve(sum, inputs, length - 1);
 
         assertEquals(22, actual);
+    }
+
+    @Test
+    public void iterate_array_number() {
+        Integer[] numbers = new Integer[]{8, 9, 9, 9, 9, 9};
+        List<Integer> list = Arrays.asList(numbers);
+        StringBuilder actual = new StringBuilder();
+        for (Integer number : result(list, 1)) {
+            actual.append(number);
+        }
+
+        assertEquals("900000", actual.toString());
+    }
+
+    @Test
+    public void iterateZero_array_number() {
+        Integer[] numbers = new Integer[]{9, 9, 9, 9, 9, 9};
+        List<Integer> list = Arrays.asList(numbers);
+        StringBuilder actual = new StringBuilder();
+        for (Integer number : result(list, 1)) {
+            actual.append(number);
+        }
+
+        assertEquals("000000", actual.toString());
+    }
+
+    public List<Integer> result(List<Integer> numbers, int idx) {
+        int listSize = numbers.size();
+        int currentIdx = listSize - idx;
+        if (numbers.get(currentIdx) == 9) {
+            numbers.set(currentIdx, 0);
+            if (currentIdx == 0) {
+                return numbers;
+            }
+            idx++;
+            return result(numbers, idx);
+        }
+        numbers.set(currentIdx, numbers.get(currentIdx) + 1);
+        return numbers;
     }
 
     private int solve(int sum, int[] inputs, int lastIndex) {
